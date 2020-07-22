903cc872f6b9c15b0a46269e6a06534ea8a20d7d
Asmee Dhungana <dhungana.asmee99@gmail.com>
2020-07-22 10:36:41 +0545

    initial state

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,9 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
-import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 
 contract SimpleStorage is Initializable {
-    using SafeMath for uint256;
-
     uint256 storedValue;
-    uint256 addedValue;
 
     function initialize () public initializer {
         storedValue = 0;
@@ -18,10 +14,6 @@ contract SimpleStorage is Initializable {
         return storedValue;
     }
 
-    function incrementValue(uint256 x) public {
-        addedValue = x;
-        storedValue.add(x);
-    }
 }
 
 
